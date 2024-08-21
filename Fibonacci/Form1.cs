5449a595cce324fe09e9aa5c3240f9b858f79bd9
MellerPeller <melker.eriksson@skola.sala.se>
2024-08-21 15:19:31 +0200

    Add project files.

---
+++ Fibonacci/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double number1 =0;
+        double number2 =1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            for (int i = 0; i < 30; i++)
+            {
+                textBox1.Text = textBox1.Text + number1.ToString()+ " ";
+                number1 = number1 + number2;
+                textBox1.Text = textBox1.Text + number2.ToString() + " ";
+                number2 = number2 + number1;
+            }
+        }
+    }
+}
